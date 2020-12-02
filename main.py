8bcf284a413885afa5908aa5d22a514cf3c21044
AlvarDev <alvardev.lp@gmail.com>
2020-12-02 00:47:38 -0300

    Secrets Manager implemented

--- main.py
+++ main.py
@@ -17,8 +17,65 @@ Adapted from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/mas
 """
 
 from flask import Flask, jsonify
+import sqlalchemy
+import google.auth
+from google.cloud import secretmanager
 
+_, PROJECT_ID = google.auth.default()
 app = Flask(__name__)
+
+
+def init_connection_engine():
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    db_user_secret = f"projects/{PROJECT_ID}/secrets/db_user_secret/versions/latest"
+    db_password_secret = f"projects/{PROJECT_ID}/secrets/db_password_secret/versions/latest"
+    db_name_secret = f"projects/{PROJECT_ID}/secrets/db_name_secret/versions/latest"
+    cloud_sql_conn_name_secret = f"projects/{PROJECT_ID}/secrets/cloud_sql_connection_name_secret/versions/latest"
+
+    # Access the secret version.
+    db_user_response = client.access_secret_version(request={"name": db_user_secret})
+    db_password_response = client.access_secret_version(request={"name": db_password_secret})
+    db_name_response = client.access_secret_version(request={"name": db_name_secret})
+    cloud_sql_conn_name_response = client.access_secret_version(request={"name": cloud_sql_conn_name_secret})
+
+    db_user = db_user_response.payload.data.decode("UTF-8")
+    db_pass = db_password_response.payload.data.decode("UTF-8")
+    db_name = db_name_response.payload.data.decode("UTF-8")
+    db_socket_dir = '/cloudsql'
+    cloud_sql_connection_name = cloud_sql_conn_name_response.payload.data.decode("UTF-8")
+
+    db_config = {
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 30,  # 30 seconds
+        "pool_recycle": 1800,  # 30 minutes
+    }
+
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,
+            password=db_pass,
+            database=db_name,
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,
+                    cloud_sql_connection_name)
+            }
+        ),
+        **db_config
+    )
+
+    return pool
+
+
+@app.before_first_request
+def create_connection():
+    global db
+    db = init_connection_engine()
 
 
 @app.route("/", methods=['GET'])
@@ -26,6 +83,12 @@ def get_pets():
     """
     This method returns a list of pets from DB on Cloud SQL
     """
+    pets = []
+
+    with db.connect() as conn:
+        pets_result = conn.execute("SELECT id, name from pets;").fetchall()
+        for row in pets_result:
+            pets.append({"id": row[0], "name": row[1]})
 
     # Response
-    return jsonify({'success': True}), 200
+    return jsonify(pets), 200
